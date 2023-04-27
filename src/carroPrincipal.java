bd27a50e519f0d2cfffe9474dba34cda23c0ba17
Elaine <e.cristina00@gmail.com>
2023-04-27 10:50:24 -0300

    Atribuindo objeto na composição.

--- src/carroPrincipal.java
+++ src/carroPrincipal.java
@@ -8,10 +8,18 @@ public class carroPrincipal {
         carro.setAnoFabricacao(2019);
         carro.setCor("cinza");
 
+        carro.proprietario = new Proprietario();
+        carro.proprietario.nome = "Elaine";
+        carro.proprietario.cpf = "123,456,789-10";
+        carro.proprietario.anoNascimento = 1986;
+
         System.out.println(carro.getFabricante());
         System.out.println(carro.getModelo());
         System.out.println(carro.getCor());
         System.out.println(carro.getAnoFabricacao());
+        System.out.println(carro.proprietario.nome);
+        System.out.println(carro.proprietario.cpf);
+        System.out.println(carro.proprietario.anoNascimento);
 
     }
 }
