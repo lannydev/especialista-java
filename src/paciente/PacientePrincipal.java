76fbe50b83ec2dc490d9e2383fe10a3ce305fdc6
Elaine <e.cristina00@gmail.com>
2023-05-05 16:19:58 -0300

    Usando o Scanner.

--- src/paciente/PacientePrincipal.java
+++ src/paciente/PacientePrincipal.java
@@ -1,11 +1,24 @@
 package paciente;
 
+import java.util.Scanner;
+
 public class PacientePrincipal {
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         CalculadoraImc paciente = new CalculadoraImc();
+        System.out.println("Informe seu peso");
+        double peso = sc.nextDouble();
+        System.out.println("Informe sua altura");
+        double altura = sc.nextDouble();
 
 
-        IndiceMassaCorporal meuImc = paciente.calcular(72.5, 1.65);
+        IndiceMassaCorporal meuImc = paciente.calcular(peso, altura);
+
+
+        sc.close();
 
     }
 }
