6dd4b198e529b2bc759d42394aeb07a6c1fc2956
Elaine <e.cristina00@gmail.com>
2023-05-14 20:16:35 -0300

    Implementando métodos genéricos.

---
+++ src/clientes/Cliente.java
@@ -0,0 +1,31 @@
+package clientes;
+
+public class Cliente {
+    private String razaoSocial;
+    private double faturamentoMensal;
+
+    public Cliente(){
+
+    }
+
+    public Cliente(String razaoSocial, double faturamentoMensal) {
+        this.razaoSocial = razaoSocial;
+        this.faturamentoMensal = faturamentoMensal;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public double getFaturamentoMensal() {
+        return faturamentoMensal;
+    }
+
+    public void setFaturamentoMensal(double faturamentoMensal) {
+        this.faturamentoMensal = faturamentoMensal;
+    }
+}
---
+++ src/clientes/ClientePrincipal.java
@@ -0,0 +1,18 @@
+package clientes;
+
+public class ClientePrincipal {
+    public static void main(String[] args) {
+        String[] nomes = {"Elaine", "Diego", "Lara"};
+        String nomeSorteado = Sorteador.<String>sortear(nomes);
+        System.out.println(nomeSorteado);
+
+        Cliente [] clientes = {
+                new Cliente("LC Personalizados e Variedades", 2_000_000),
+                new Cliente("L.Cruz Cosméticos", 25_000_000),
+                new Cliente("L.C Artes", 3_000_000),
+        };
+
+        Cliente clienteSorteado = Sorteador.<Cliente>sortear(clientes);
+        System.out.println(clienteSorteado.getRazaoSocial());
+    }
+}
