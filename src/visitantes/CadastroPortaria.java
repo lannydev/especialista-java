77133ddbc5cc69a0dab36d2cdb374991fe431e35
Elaine <e.cristina00@gmail.com>
2023-05-26 16:33:26 -0300

    Sobrecarga de métodos.

--- src/visitantes/CadastroPortaria.java
+++ src/visitantes/CadastroPortaria.java
@@ -2,8 +2,15 @@ package visitantes;
 
 public class CadastroPortaria {
 
-    void cadastrar(Visitante visitante, final int tempoDeExpiraçãoEmMeses){
-        final int tempoDeExpiracaoEmDias = tempoDeExpiraçãoEmMeses * 30;
+    static final int TEMPO_DE_EXPIRACAO_PADRAO_EM_MESES = 1;
+
+    void cadastrar(final Visitante visitante){
+        this.cadastrar(visitante, TEMPO_DE_EXPIRACAO_PADRAO_EM_MESES);
+
+    }
+
+    void cadastrar(Visitante visitante, final int tempoDeExpiracaoEmMeses){
+        final int tempoDeExpiracaoEmDias = tempoDeExpiracaoEmMeses * 30;
 
         System.out.printf("Acesso para o visitante %s cadastrado por %d dias",
                 visitante.nome, tempoDeExpiracaoEmDias);
