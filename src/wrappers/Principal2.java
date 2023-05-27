7c49965fbbb77a1284bc5e5d20b47381d968d57e
Elaine <e.cristina00@gmail.com>
2023-05-27 19:27:01 -0300

    Autoboxing e unboxing.

---
+++ src/wrappers/Principal2.java
@@ -0,0 +1,12 @@
+package wrappers;
+
+public class Principal2 {
+    public static void main(String[] args) {
+
+        Integer diasDoMês= Integer.valueOf(30);
+
+        // pode ser feito de forma literal também, pois o próprio compilador faz a concersão;
+        Integer diaDoMes = 25;
+
+    }
+}
