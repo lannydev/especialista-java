26b4784cb1fe08145df1797a059e3c08500b509a
Elaine <e.cristina00@gmail.com>
2023-05-28 01:56:48 -0300

    Acessando a atribuindo elementos de arrays.

--- src/arrays/ArraysPrincipal.java
+++ src/arrays/ArraysPrincipal.java
@@ -7,8 +7,12 @@ public class ArraysPrincipal {
        // double[]notas = new double[posicaoDasNotas];
 
         double[] notas = new double[]{9.5, 9.0, 7.01, 5.0};
+        notas[3] = 6.5;
+        System.out.println(notas[3]);
 
         double[] minhasNotas = {6.0, 7.1, 6.8, 9.5};
+
+
 
 
 
