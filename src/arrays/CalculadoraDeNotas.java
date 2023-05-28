ddff1ddf151a2d1d693f8fc32132249f60e49c46
Elaine <e.cristina00@gmail.com>
2023-05-28 02:18:15 -0300

    Outra forma de utilizar o for para calcular a média.

--- src/arrays/CalculadoraDeNotas.java
+++ src/arrays/CalculadoraDeNotas.java
@@ -4,10 +4,17 @@ public class CalculadoraDeNotas {
 
     static double calcularMedia(double[] numeros){
         double total = 0;
-        for (int i = 0; i < numeros.length; i++) {
-            total += numeros[i];
+
+        for (double numero: numeros) {
+            total += numero;
 
         }
+
+        /*for (int i = 0; i < numeros.length; i++) {
+
+        }*/
+
+
         return total / numeros.length;
     }
 }
