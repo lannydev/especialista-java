7de1272cdee2e37e53bde521abc41441eef62972
Elaine <e.cristina00@gmail.com>
2023-05-28 01:50:49 -0300

    Declarando e iniciando arrays.

---
+++ src/arrays/ArraysPrincipal.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class ArraysPrincipal {
+    public static void main(String[] args) {
+
+        //int posicaodasNotas = 4;
+       // double[]notas = new double[posicaoDasNotas];
+
+        double[] notas = new double[]{9.5, 9.0, 7.01, 5.0};
+
+        double[] minhasNotas = {6.0, 7.1, 6.8, 9.5};
+
+
+
+    }
+}
